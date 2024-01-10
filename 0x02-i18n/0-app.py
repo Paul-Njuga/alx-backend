b6f07ec98d145e1119fc27fdbe313ae2ec991e0f
Paul-Njuga <tyrosavant@gmail.com>
2024-01-10 04:27:44 +0300

    Add flask entry point

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/evn python3
+"""
+Flask App
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello() -> str:
+    """Hello route"""
+    return render_template('0-indes.html')
+
+if __name__ = "__main__":
+    app.run(host="0.0.0.0", port="5000")
