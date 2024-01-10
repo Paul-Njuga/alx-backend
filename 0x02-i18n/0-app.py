30265e32978890257695affdf5178e4782ff8882
Paul-Njuga <tyrosavant@gmail.com>
2024-01-10 04:32:05 +0300

    Fix: edit for pep8

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,12 +4,15 @@ Flask App
 """
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=["GET"], strict_slashes=False)
 def hello() -> str:
     """Hello route"""
-    return render_template('0-indes.html')
+    return render_template('0-index.html')
+
 
 if __name__ = "__main__":
     app.run(host="0.0.0.0", port="5000")
